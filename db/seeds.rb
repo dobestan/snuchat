3db94100d2ced5cff1c800c2123e36c2d059e011
dobestan <dobestan@gmail.com>
2014-05-15 13:53:26 +0900

    add User seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Admin Users
+admin_users = [ { name: "dobestan", mysnu: "dobestan", admin: true, active: true, password: "foobar", password_confirmation: "foobar" } ]
+admin_users.each do |admin_user|
+  User.create(admin_user)
+end
+
+demo_users = [ { name: "demo1", mysnu: "demo1", active: true, password: "foobar", password_confirmation: "foobar" },
+               { name: "demo2", mysnu: "demo2", active: true, password: "foobar", password_confirmation: "foobar" } ]
+demo_users.each do |demo_user|
+  User.create(demo_user)
+end
+
